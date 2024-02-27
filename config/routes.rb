2fc02d032afea2513b7a6750e0b25ea50536b38f
hydaline-code <djougancharlen@gmail.com>
2024-02-27 12:12:37 +0100

    add identification endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,20 @@
-Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+# Rails.application.routes.draw do
+#   devise_for :users
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+
+#   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+#   # Defines the root path route ("/")
+#   # root "articles#index"
+# end
+Rails.application.routes.draw do
+  devise_for :users, path: '', path_names: {
+    sign_in: 'login',
+    sign_out: 'logout',
+    registration: 'signup'
+  },
+  controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
 end
