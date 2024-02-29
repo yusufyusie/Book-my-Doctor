4ad334c233f76f951e32dd05cdab29f6742a7995
hydaline-code <djougancharlen@gmail.com>
2024-02-29 20:22:11 +0100

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  post '/login', to: 'sessions#create'
+ post '/signup', to: 'registrations#create'
+ delete '/logout', to: 'sessions#destroy'
+
 end
