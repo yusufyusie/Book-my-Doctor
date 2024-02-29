fac39c37b99172ff8a91a03a490f2b9d292b3e73
hydaline-code <djougancharlen@gmail.com>
2024-02-29 20:18:01 +0100

    Add user.rb

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  has_secure_password
+  has_many :doctors, dependent: :destroy
+  has_many :appointments, dependent: :destroy
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :password, presence: true
+ end
