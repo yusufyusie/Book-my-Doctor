09e50511f7e54ddb2858df0a96ce3306d16dc326
hydaline-code <djougancharlen@gmail.com>
2024-03-03 07:50:54 +0100

    Update for the doctor_api

--- app/models/json_web_token.rb
+++ app/models/json_web_token.rb
@@ -1,10 +1,10 @@
-class JsonWebToken
-  DEFAULT_SECRET_KEY = 'your_default_secret_key'.freeze
- def self.encode(payload)
-  expiration = 300.hours.from_now.to_i
- JWT.encode(payload.merge(exp: expiration), ENV.fetch('DEVISE_JWT_SECRET_KEY', DEFAULT_SECRET_KEY), 'HS256')
- end
- def self.decode(token)
- JWT.decode(token, ENV.fetch('DEVISE_JWT_SECRET_KEY', DEFAULT_SECRET_KEY), true, algorithm: 'HS256').first
- end
-end
+
+  class JsonWebToken
+    def self.encode(payload)
+     expiration = 30.days.from_now.to_i
+    JWT.encode(payload.merge(exp: expiration), ENV.fetch('DEVISE_JWT_SECRET_KEY', Rails.application.secret_key_base), 'HS256')
+    end
+    def self.decode(token)
+    JWT.decode(token, ENV.fetch('DEVISE_JWT_SECRET_KEY', Rails.application.secret_key_base), true, algorithm: 'HS256').first
+   end
+  end
