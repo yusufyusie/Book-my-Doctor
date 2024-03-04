fcb24afe6ca4ed28f0cc22f9d7af117b4bfe4408
yusufyusie <yesuf023@gmail.com>
2024-03-04 06:27:57 +0300

    Add Appintment model

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,4 +1,10 @@
 class Appointment < ApplicationRecord
   belongs_to :doctor
   belongs_to :user
+
+  validates :date, presence: true, allow_blank: false
+  validates :time, presence: true, allow_blank: false
+  validates :doctor, presence: true, allow_blank: false
+  validates :user, presence: true, allow_blank: false
+  validates :reason, presence: true, allow_blank: false
 end
