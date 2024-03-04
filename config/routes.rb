c911d6e3e12bfea27bb37aa5fb57b06afad5be1e
yusufyusie <yesuf023@gmail.com>
2024-03-04 04:09:52 +0300

    create a AppointmentsController

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,11 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
+      get 'appointments/index'
+      get 'appointments/show'
+      get 'appointments/create'
+      get 'appointments/update'
+      get 'appointments/destroy'
       post '/login', to: 'sessions#create'
       post '/signup', to: 'registrations#create'
       resources :doctors, only: [:index, :create, :show, :destroy]
