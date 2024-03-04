a6cb455d7531e0d0ad73bfc6594976fffe341f6f
yusufyusie <yesuf023@gmail.com>
2024-03-04 04:11:57 +0300

    Update a AppointmentsController

--- app/controllers/api/v1/appointments_controller.rb
+++ app/controllers/api/v1/appointments_controller.rb
@@ -1,16 +1,56 @@
-class Api::V1::AppointmentsController < ApplicationController
-  def index
-  end
+module Api
+  module V1
+    class AppointmentsController < ApplicationController
+      before_action :set_appointment, only: [:show, :update, :destroy]
 
-  def show
-  end
+      # GET /appointments
+      def index
+        @appointments = Appointment.where(user_id: @current_user.id).order(created_at: :desc)
+        render json: @appointments
+      end
 
-  def create
-  end
+      # GET /appointments/:id
+      def show
+        render json: @appointment
+      end
 
-  def update
-  end
+      # POST /appointments
+      def create
+        @appointment = Appointment.new(appointment_params)
+        @appointment.user_id = @current_user.id
 
-  def destroy
+        if @appointment.save
+          render json: @appointment, status: :created
+        else
+          render json: @appointment.errors, status: :unprocessable_entity
+        end
+      end
+
+      # PATCH/PUT /appointments/:id
+      def update
+        if @appointment.update(appointment_params)
+          render json: @appointment
+        else
+          render json: @appointment.errors, status: :unprocessable_entity
+        end
+      end
+
+      # DELETE /appointments/:id
+      def destroy
+        @appointment.destroy
+      end
+
+      private
+
+      # Use callbacks to share common setup or constraints between actions.
+      def set_appointment
+        @appointment = Appointment.find(params[:id])
+      end
+
+      # Only allow a trusted parameter "white list" through.
+      def appointment_params
+        params.require(:appointment).permit(:doctor_id, :date, :time, :reason)
+      end
+    end
   end
 end
