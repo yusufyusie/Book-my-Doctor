406fbb9572dd5c81d0436f2cb6128e7072793c6f
yusufyusie <yesuf023@gmail.com>
2024-03-04 05:54:35 +0300

    Add appintment get

--- config/routes.rb
+++ config/routes.rb
@@ -4,17 +4,11 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      get 'appointments/index'
-      get 'appointments/show'
-      get 'appointments/create'
-      get 'appointments/update'
-      get 'appointments/destroy'
       post '/login', to: 'sessions#create'
       post '/signup', to: 'registrations#create'
       resources :doctors, only: [:index, :create, :show, :destroy]
       resources :test, only: [:index]
-      resources :workspaces, only: [:index, :show, :create, :destroy]
-      resources :reservations, only: [:index, :create] do
+      resources :appointments, only: [:index, :create, :show, :update, :destroy] do
         collection do
           get ':id', action: :show
         end
