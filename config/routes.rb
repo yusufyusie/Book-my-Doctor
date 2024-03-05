ed2f7038272908c8fc861f82c1110bf085f4abee
hydaline-code <107451859+hydaline-code@users.noreply.github.com>
2024-03-05 16:16:44 +0100

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-   root "doctors_controller#index"
+   root "doctors#index"
   post '/login', to: 'sessions#create'
   post '/signup', to: 'registrations#create'
  
