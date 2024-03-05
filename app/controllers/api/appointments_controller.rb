6e7ac1dfb6d0a7d2c933dbb3caf336c4f4955cc1
hydaline-code <djougancharlen@gmail.com>
2024-03-05 06:02:05 +0100

    Update appointments

--- app/controllers/api/appointments_controller.rb
+++ app/controllers/api/appointments_controller.rb
@@ -1,33 +1,9 @@
 class Api::AppointmentsController < ApplicationController
   before_action :authenticate_token!, only: %i[index show create destroy]
 
-  def index
-    appointments = @current_user.appointments.includes(:doctor)
-    
-    if appointments.empty?
-      render json: { error: 'No appointments found' }, status: :not_found
-    else
-      appointments_details = appointments.map do |appointment|
-        {
-          id: appointment.id,
-          doctor: {
-            name: appointment.doctor.name,
-            specialization: appointment.doctor.specialization,
-            cost: appointment.doctor.cost,
-            image_url: appointment.doctor.image_url
-          },
-          user_id: appointment.user_id,
-          date_of_appointment: appointment.date_of_appointment,
-          imageUrl: appointment.doctor.image_url
-        }
-      end
-      
-      render json: { data: appointments_details, message: 'Appointments loaded successfully' }, status: :ok
-    end
-  end
-  
   def show
   end
+
   # def create
   #   appointment = Appointment.new(appointment_params)
   
@@ -45,6 +21,12 @@ class Api::AppointmentsController < ApplicationController
 
   def create
     appointment = current_user.appointments.build(appointment_params)
+
+    if appointment.status == "canceled"
+      render json: { error: "Appointment creation canceled" }, status: :unprocessable_entity
+      return
+    end
+
     if appointment.save
       response = {
         doctor_id: appointment.doctor_id,
@@ -56,20 +38,6 @@ class Api::AppointmentsController < ApplicationController
     end
   end
 
-
-  def destroy
-    appointment = current_user.appointments.find_by(id: params[:id])
-
-    if appointment
-      if appointment.destroy
-        render json: { message: 'Appointment deleted successfully' }, status: :ok
-      else
-        render json: { error: 'Failed to delete appointment' }, status: :unprocessable_entity
-      end
-    else
-      render json: { error: 'Appointment not found' }, status: :not_found
-    end
-  end
 
   private
   def appointment_params
