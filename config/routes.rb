fb5f97f74155583435a208186e2dd3f8a1a617ae
hydaline-code <107451859+hydaline-code@users.noreply.github.com>
2024-03-05 15:52:38 +0100

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+   root "doctors#index"
   post '/login', to: 'sessions#create'
- post '/signup', to: 'registrations#create'
+  post '/signup', to: 'registrations#create'
  
  namespace :api do
   resources :doctors, only: [:index, :show, :create, :destroy]
