aa454d4a6277b41bc2d9c2f7b20180c73471efff
Fombi Favour <fombifavour@gmail.com>
2024-03-06 08:45:43 +0100

    Update 20240303110734_create_appointments.rb

--- db/migrate/20240303110734_create_appointments.rb
+++ db/migrate/20240303110734_create_appointments.rb
@@ -4,7 +4,6 @@ class CreateAppointments < ActiveRecord::Migration[7.0]
       t.references :user, null: false, foreign_key: true
       t.references :doctor, null: false, foreign_key: true
       t.datetime :date_of_appointment
-      t.string :status
 
       t.timestamps
     end
