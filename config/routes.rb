783dbc1786a3c32d31840b273a8625c3e04f4874
hydaline-code <djougancharlen@gmail.com>
2024-03-06 14:07:35 +0100

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,15 @@ Rails.application.routes.draw do
     get 'appointments/index'
     get 'appointments/show'
     get 'appointments/create'
-    get 'appointments/update'
     get 'appointments/destroy'
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
-   # root "doctors#index"
+  
+    namespace :api do
+      root "doctors#index"
+    end
+
   post '/login', to: 'sessions#create'
   post '/signup', to: 'registrations#create'
  
